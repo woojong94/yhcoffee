3dcd1b68905a55dcc791bffa372c7a7a9ff2f633
Unknown123465 <86813264+Unknown123465@users.noreply.github.com>
2021-08-12 09:54:52 +0900

    yh4

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-	return res.render("main/index");	// main -> views 폴더 기준
-});
-
-module.exports = router;
